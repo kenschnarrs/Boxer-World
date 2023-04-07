d1e84e051e5ff7436da94148da2c314c90c71d53
DaronAdams <daron.cole@icloud.com>
2023-04-06 20:40:05 -0500

    Updated user migrations/model

--- db/schema.rb
+++ db/schema.rb
@@ -45,9 +45,29 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_014155) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phone", default: "", null: false
+    t.text "blurb", default: "", null: false
+    t.integer "card_num", default: 0, null: false
+    t.integer "cvc", default: 0, null: false
+    t.integer "exp_month", default: 0, null: false
+    t.integer "exp_year", default: 0, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true

24988dc6ca6859b45f78d34dd48ae4bb44fa6ce5
DaronAdams <daron.cole@icloud.com>
2023-04-09 10:55:07 -0500

    started styling results

--- db/migrate/20230408203207_create_items.rb
+++ db/migrate/20230408203207_create_items.rb
@@ -5,7 +5,6 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.string :category, null: false, default: ""
       t.string :brand, null: false, default: ""
       t.string :size, null: false, default: ""
-      t.string :image_path, null: false, default: ""
       t.integer :num_in_stock, null: false, default: 0
       t.boolean :isFeatured, null: false, default: false
       t.decimal :rating, null: false, default: 0.0

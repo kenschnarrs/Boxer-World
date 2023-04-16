484f60af73dcdab90ce6b0bcf19c1116643129fa
DaronAdams <daron.cole@icloud.com>
2023-04-15 20:33:57 -0500

    fixed search results page and added items

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,6 +40,57 @@ item2 = Item.create!(
     price: 30.00,
     rating: 0
 )
+
+
+item3 = Item.create!(
+  name: "Adult Gloves",
+  brand: "Adidas",
+  category: "Boxing Gloves",
+  size: "Large",
+  isFeatured: false,
+  num_in_stock: 100,
+  price: 50.00,
+  rating: 4.5
+)
+
+# Item 2 (using the original format)
+item4 = Item.create!(
+  name: "Kid Gloves",
+  brand: "Nike",
+  category: "Boxing Gloves",
+  size: "Small",
+  isFeatured: true,
+  num_in_stock: 50,
+  price: 30.00,
+  rating: 0
+)
+
+# Item 3
+item5 = Item.create!(
+  name: "Training Gloves",
+  brand: "Everlast",
+  category: "Boxing Gloves",
+  size: "Medium",
+  isFeatured: true,
+  num_in_stock: 75,
+  price: 40.00,
+  rating: 3.8
+)
+
+# Item 4
+item6 = Item.create!(
+  name: "Competition Gloves",
+  brand: "Title Boxing",
+  category: "Boxing Gloves",
+  size: "Large",
+  isFeatured: false,
+  num_in_stock: 60,
+  price: 75.00,
+  rating: 4.9
+)
+
+
+
 item2.picture.attach(
     io: File.open(Rails.root.join('app', 'assets', 'images', 'adult_gloves.jpeg')), 
     filename: 'adult_gloves.jpeg', 

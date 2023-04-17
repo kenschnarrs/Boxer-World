468e8dd64374e373e89e9a9e86ec24c853e8349c
kenschnarrs <knschnarrs@gmail.com>
2023-04-16 21:40:03 -0500

    added home page and started categories work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,23 @@ class PagesController < ApplicationController
 
     # Renders the home page
     def home
+        @items = Item.where(isFeatured: true)
         render :home
+    end
+
+    def boxing_gloves
+        @items = Item.where(category: "Boxing Gloves")
+        render :category
+    end
+
+    def punching_bags
+        @items = Item.where(category: "Punching bags")
+        render :category
+    end
+
+    def apparel
+        @items = Item.where(category: "Apparel")
+        render :category
     end
 
 end

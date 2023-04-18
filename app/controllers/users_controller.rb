ee2e171365b893712fce2f1a6d77bbb516ae0ac4
Calvin Gott <clgott@memphis.edu>
2023-04-17 23:55:46 -0500

    Updated V9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,29 +8,45 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
+        @user = current_user
         render :show
     end
 
     def edit
-        @user = User.find(params[:id])
+        @user = current_user
         render :edit
     end
 
     def update
-        @user = User.find(params[:id])
-        if @user.update(params.require(:user).permit(:email, :password))
+        @user = current_user
+        if @user.update(params.require(:user).permit(:name,:email, :phone, :address,:card_num, :cvc, :exp_month, :exp_year))
           flash[:success] = 'Your Account updated!'
-          redirect_to user_url(@user)
+          redirect_to root_path
         else
-          flash.now[:error] = 'TYour Account updated failed'
+          flash.now[:error] = 'Your Account updated failed'
           render :edit, status: :unprocessable_entity
         end
     end
 
     def billing
-        @user = User.find(params[:id])
+        @user = current_user
         render :billing
+    end
+
+    def profile
+      @user = current_user
+      render :profile
+    end
+
+    def update_profile
+      @user = current_user
+      if @user.update(params.require(:user).permit(:username, :blurb))
+        flash[:success] = 'Your Account updated!'
+        redirect_to root_path
+      else
+        flash.now[:error] = 'Your Account updated failed'
+        render :edit, status: :unprocessable_entity
+      end
     end
     
 end

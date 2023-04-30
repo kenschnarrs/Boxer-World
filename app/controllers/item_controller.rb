67374185ac086d5deb452c1470d71ce7f0e4faa0
kenschnarrs <knschnarrs@gmail.com>
2023-04-29 20:43:47 -0500

    fixed up item show bug, delte review authorization bug

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,16 +1,22 @@
 class ItemController < ApplicationController
     
     def show
+
         @item = Item.find(params[:id])
-
-        cart_item = current_user.cart.cart_items.find_by(item_id: params[:id])
-
         @stock = @item.num_in_stock
-        
-        # You can still view an item that is out-of-stock,
-        # you just can't add it to your cart.
-        if cart_item.present?
-            @stock = @item.num_in_stock - cart_item.quantity
+
+        unless current_user.nil?
+            if current_user.cart.nil?
+                current_user.cart = Cart.new
+            else
+                cart_item = current_user.cart.cart_items.find_by(item_id: params[:id])
+    
+                # You can still view an item that is out-of-stock,
+                # you just can't add it to your cart.
+                if cart_item.present?
+                    @stock = @item.num_in_stock - cart_item.quantity
+                end
+            end
         end
 
         render :show
